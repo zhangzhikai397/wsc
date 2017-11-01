9f6bd7b0f7d4e2fc32fa379079741b75a05d3555
stassyn <stasinyavskiy@gmail.com>
2017-11-01 17:31:16 +0200

    asd

--- send.py
+++ send.py
@@ -5,7 +5,7 @@ from random import randint
 
 
 if __name__ == '__main__':
-    w = WSC('key')
+    w = WSC('key', hostname='btcgo.org', ssl=True)
     while True:
         print(w.send('main/room', 'Hello' + str(randint(0, 999999))).raw)
         print(w.stat('main/room').raw)
